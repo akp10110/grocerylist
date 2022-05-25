84fa0cea68d6fd85a7373e1f7cf6423b7ee0d640
akp10110 <akp10110@gmail.com>
2022-05-25 22:24:35 +1000

    Initial Commit

---
+++ grocerylist/grocerylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  grocerylistApp.swift
+//  grocerylist
+//
+//  Created by Remya Pillai on 25/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct grocerylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
