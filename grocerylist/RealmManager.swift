991464a999e5a23de46a6991d58c4d4ddd07d33a
akp10110 <akp10110@gmail.com>
2022-05-30 16:57:00 +1000

    Added realm-swift package and RealmManager

---
+++ grocerylist/RealmManager.swift
@@ -0,0 +1,29 @@
+//
+//  RealmManager.swift
+//  grocerylist
+//
+//  Created by Anish Pillai on 30/5/2022.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmManager: ObservableObject {
+    private(set) var localRealm: Realm?
+    
+    init() {
+        openRealm()
+    }
+    
+    func openRealm() {
+        do {
+            let config = Realm.Configuration(schemaVersion: 1)
+            Realm.Configuration.defaultConfiguration = config
+            
+            localRealm = try Realm()
+            
+        } catch {
+            print("Error opening Realm: \(error)")
+        }
+    }
+}
